b1e8156a869778be826db264fd2d4c817cc9ba90
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-07 23:56:19 +0200

    Start Lab-3.3

---
+++ TwoWeekLab-3.3/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	int words = 0, nStr = 0;
+	cout << "Enter string:" << endl << " >  ";
+	getline(cin, str);
+	cout << endl;
+
+	
+	
+	system("pause");
+	return NULL;
+}
