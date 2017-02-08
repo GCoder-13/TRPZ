d806c6e2c459003094fca45f76160385fe88c06a
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-08 23:51:15 +0200

    Lab-3.3 Testing program

--- TwoWeekLab-3.3/Source.cpp
+++ TwoWeekLab-3.3/Source.cpp
@@ -6,11 +6,18 @@ using namespace std;
 int main()
 {
 	string str;
-	int word = 0, nStr = 0;
+	int word = 1, nStr = 0;
 
 	cout << "Enter string:" << endl << " >  ";
 	getline(cin, str);
 	cout << endl;
+
+	if (str.empty())
+	{
+		cout << "You do not enter string!" << endl;
+		system("pause");
+		return NULL;
+	}
 
 	for (int i(1); i < str.length(); i++)
 	{
@@ -19,18 +26,17 @@ int main()
 		else if ((str[i] == ' ' && str[i - 1] == ' ') || str[i] == ',' || str[i] == ';' || str[i] == ':')
 			continue;
 		else if (str[i] == ' ')
-			word++;
-		else if (j == str.length()-1 && str[j] != ' ')
-			word++;
+			word++;			
 
-	/*	if (str[i] == '.' || str[i] == '?' || str[i] == '!' || i == str.length()-1)
+		if (str[i] == '.' || str[i] == '?' || str[i] == '!' || i == str.length()-1)
 		{
+			if(str[i-1] == ' ')
+				word--;
 			nStr++;
 			
 			cout << "String " << nStr << ": " << word << " words" << endl;
 			word = 0;
 		}
-	*/
 	}
 	
 	system("pause");
