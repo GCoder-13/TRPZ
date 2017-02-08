e0262966561c3d8c35fb80e38ad4563654a94a68
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-08 22:10:54 +0200

    Lab-3.3 Програма рахує килькість слів

--- TwoWeekLab-3.3/Source.cpp
+++ TwoWeekLab-3.3/Source.cpp
@@ -6,12 +6,32 @@ using namespace std;
 int main()
 {
 	string str;
-	int words = 0, nStr = 0;
+	int word = 0, nStr = 0;
+
 	cout << "Enter string:" << endl << " >  ";
 	getline(cin, str);
 	cout << endl;
 
-	
+	for (int i(1); i < str.length(); i++)
+	{
+		if (i - 1 == 0 && str[i - 1] != ' ' && str[i] == ' ')
+			word++;
+		else if ((str[i] == ' ' && str[i - 1] == ' ') || str[i] == ',' || str[i] == ';' || str[i] == ':')
+			continue;
+		else if (str[i] == ' ')
+			word++;
+		else if (j == str.length()-1 && str[j] != ' ')
+			word++;
+
+	/*	if (str[i] == '.' || str[i] == '?' || str[i] == '!' || i == str.length()-1)
+		{
+			nStr++;
+			
+			cout << "String " << nStr << ": " << word << " words" << endl;
+			word = 0;
+		}
+	*/
+	}
 	
 	system("pause");
 	return NULL;
