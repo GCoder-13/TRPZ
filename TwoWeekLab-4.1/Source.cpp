a2d76314ca220238e8052e3a90a03df79cba9cf2
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-10 00:14:47 +0200

    Lab-4.1 Сортування по імені

--- TwoWeekLab-4.1/Source.cpp
+++ TwoWeekLab-4.1/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -16,26 +17,60 @@ void Input(ABITURIENT*& A, const int n)
 	for (int i(0); i < n; i++)
 	{
 		cout << "Enter name: ";
-		cin >> A[i].NAME;
+		getline(cin, A[i].NAME);
 		cout << "Enter gender (M or W): ";
 		cin >> A[i].GENDER;
+		cin.ignore();
 		cout << "Enter ocupation: ";
-		cin >> A[i].SPEC;
-		cout << "Enter score for exams: ";
+		getline(cin, A[i].SPEC);
+		cout << "Enter score for exams: " << endl;
 		for (int j(0); j < 3; j++)
 		{
 			cout << " >  ";
 			cin >> A[i].EXAM[j];
 		}
+		cin.ignore();
+	}
+}
+
+void Output(const ABITURIENT* A, const int n)
+{
+	cout << setw(10) << "NAME" << setw(16) << "GENDER" << setw(16) << "OCUPATION" << setw(13) << "EXEM" << endl;
+	cout << "------------------------------------------------------------------" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << setw(16) << A[i].NAME << setw(8) << A[i].GENDER << setw(20) << A[i].SPEC << '\t';
+		for (int j = 0; j < 3; j++)
+		{
+			cout << setw(3) << A[i].EXAM[j] ;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Sort(ABITURIENT*& A, const int n)
+{
+	for (int i(0); i < n; i++)
+	{
+		ABITURIENT temp = A[i];
+		int j;
+		for (j = i - 1; j >= 0 && A[j].NAME > temp.NAME; j--)
+			A[j + 1] = A[j];
+		A[j + 1] = temp;
 	}
 }
 
 int main()
 {
-	const int N = 3;
-	ABITURIENT ABIT[N];
+	const int N = 1;
+	ABITURIENT* ABIT = new ABITURIENT[N];
+	Input(ABIT, N);
+	Sort(ABIT, N);
 
-	
+	cout << endl << endl;
+
+	Output(ABIT, N);
 
 	system("pause");
 	return NULL;
