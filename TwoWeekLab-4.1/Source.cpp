4d68bb0b02849fcb65ba77fdb635c7c6812ac777
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-10 12:49:51 +0200

    Lab-4.1 Update Output; Add OutputAll() and OutputPassRate()

--- TwoWeekLab-4.1/Source.cpp
+++ TwoWeekLab-4.1/Source.cpp
@@ -23,6 +23,7 @@ void Input(ABITURIENT*& A, const int n)
 		cin.ignore();
 		cout << "Enter ocupation: ";
 		getline(cin, A[i].SPEC);
+		cin.clear();
 		cout << "Enter score for exams: " << endl;
 		for (int j(0); j < 3; j++)
 		{
@@ -30,23 +31,27 @@ void Input(ABITURIENT*& A, const int n)
 			cin >> A[i].EXAM[j];
 		}
 		cin.ignore();
+		cout << "-----------------------------------" << endl;
 	}
+	system("cls");
 }
 
-void Output(const ABITURIENT* A, const int n)
+void Output(const ABITURIENT A)
 {
-	cout << setw(10) << "NAME" << setw(16) << "GENDER" << setw(16) << "OCUPATION" << setw(13) << "EXEM" << endl;
-	cout << "------------------------------------------------------------------" << endl;
-	for (int i = 0; i < n; i++)
+	cout << setw(16) << A.NAME << setw(8) << A.GENDER << setw(20) << A.SPEC << '\t';
+	for (int j = 0; j < 3; j++)
 	{
-		cout << setw(16) << A[i].NAME << setw(8) << A[i].GENDER << setw(20) << A[i].SPEC << '\t';
-		for (int j = 0; j < 3; j++)
-		{
-			cout << setw(3) << A[i].EXAM[j] ;
-		}
-		cout << endl;
+		cout << setw(3) << A.EXAM[j] ;
 	}
 	cout << endl;
+}
+
+void OutputAll(const ABITURIENT* A, const int n)
+{
+	cout << endl << setw(10) << "NAME" << setw(16) << "GENDER" << setw(16) << "OCUPATION" << setw(13) << "EXEM" << endl
+		<< "------------------------------------------------------------------" << endl;
+	for (int i(0); i < n; i++)
+		Output(A[i]);
 }
 
 void Sort(ABITURIENT*& A, const int n)
@@ -61,16 +66,42 @@ void Sort(ABITURIENT*& A, const int n)
 	}
 }
 
+void OutputPassRate(const ABITURIENT* A, const int n, int rate)
+{
+	cout << endl << setw(10) << "NAME" << setw(16) << "GENDER" << setw(16) << "OCUPATION" << setw(13) << "EXEM" << endl
+		<< "------------------------------------------------------------------" << endl;
+	for (int i(0); i < n; i++)
+	{
+		bool sw = true;
+		for (int j(2); j >= 0; j--)
+		{
+			if (A[i].EXAM[j] < rate)
+			{
+				sw = false;
+				break;
+			}
+		}
+		if (sw)
+			Output(A[i]);
+	}
+}
+
 int main()
 {
-	const int N = 1;
+	const int N = 2;
 	ABITURIENT* ABIT = new ABITURIENT[N];
 	Input(ABIT, N);
+
+	cout << "Sorting: ";
 	Sort(ABIT, N);
+	OutputAll(ABIT, N);
 
-	cout << endl << endl;
+	int rate;
+	cout << endl << "Enter pass rate: ";
+	cin >> rate;
+	OutputPassRate(ABIT, N, rate);
+	
 
-	Output(ABIT, N);
 
 	system("pause");
 	return NULL;
