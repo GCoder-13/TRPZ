b9f34ca874d03fae82dcc6d661c561dcce492c5d
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-11 19:20:26 +0200

    Lab-4.2 Function CheckSum() Cума елементів j-стовпця матриці співпадає з сумою елементів i-го рядка

--- TwoWeekLab-4.2/Source.cpp
+++ TwoWeekLab-4.2/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -28,16 +29,62 @@ void ArrInput(int**& A, const int n)
 		}
 		cout << "==============" << endl;
 	}
+	system("cls");
+}
+
+void ArrOutput(int** A, const int n)
+{
+	for (int i(0); i < n; i++)
+	{
+		for (int j(0); j < n; j++)
+			cout << setw(5) << A[i][j] << "   |";
+		cout << endl;
+	}
+}
+
+int RowSum(int** A, const int n, const int i)
+{
+	int sum = 0;
+	for (int j(0); j < n; j++)
+		sum += A[i][j];
+	return sum;
+}
+
+int ColumSum(int** A, const int n, const int j)
+{
+	int sum = 0;
+	for (int i(0); i < n; i++)
+		sum += A[i][j];
+	return sum;
+}
+
+void CheckSum(int** A, const int n)
+{
+	bool found = false;
+	for(int i(0); i < n; i++)
+		for (int j(0); j < n; j++)
+		{
+			if (RowSum(A, n, i) == ColumSum(A, n, j))
+			{
+				found = true;
+				cout << "The sum of elements row " << i + 1 << " = " << "sum of elements colum " << j + 1 << endl;
+			}
+		}
+	if (!found)
+		cout << "These elements not found" << endl;
 }
 
 int main()
 {
 	int n;
-	cout << "Enter size matrix (n, m): ";
+	cout << "Enter size matrix (n, n): ";
 	cin >> n;
 	int** arr = ArrCreate(n);
 	ArrInput(arr, n);
 
+	ArrOutput(arr, n);
+	cout << endl << "The sum of elements row = sum elements colum: " << endl;
+	CheckSum(arr, n);
 	
 
 	ArrDelete(arr, n);
