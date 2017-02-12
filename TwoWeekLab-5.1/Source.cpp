0924e863268e31d698488a8f02db0221730ebd39
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-12 22:44:33 +0200

    Lab-5.1 Finish lab

--- TwoWeekLab-5.1/Source.cpp
+++ TwoWeekLab-5.1/Source.cpp
@@ -6,22 +6,66 @@ using namespace std;
 
 void Copy(const ifstream& fin, ofstream& fout)
 {
-	fout << fin.rdbuf();
+	fout << fin.rdbuf() << endl;
 }
 
-void Print(ifstream& fin)
+void Print(ifstream& fin, ofstream& fout)
 {
 	char temp;
 	bool check = false;
+	fout << "--------------------------------------------------" << endl;
 	while (fin.good())
 	{
 		temp = fin.get();
 		if (temp == ':' && !check)
 			check = true;
 		else if (check)
+		{
 			cout << temp;
+			fout << temp;
+		}
 	}
 	cout << endl;
+	fout << endl;
+}
+
+int StringOddWords(ifstream& fin, ofstream& fout)
+{
+	char temp;
+	int word = 0;
+	int str = 0;
+	while (fin.good())
+	{
+		temp = fin.get();
+		if (temp == ' ' || temp == '.')
+			word++;
+		if (temp == '.' && (word & 1))
+		{
+			str++;
+			word = 0;
+		}
+	}
+	return str;
+}
+
+void WordDelete(ifstream& fin, ofstream& fout)
+{
+	char temp;
+	bool check = true;
+	fout << "--------------------------------------------------" << endl;
+	while (fin.good())
+	{
+		temp = fin.get();
+		if (temp == '.')
+			check = true;
+		if (temp != ',' && check)
+		{
+			cout << temp;
+			fout << temp;
+		}
+		else if (temp == ',')
+			check = false;
+	}
 }
 
 int main()
@@ -29,20 +73,28 @@ int main()
 	ifstream fin;
 	ofstream fout;
 
-	fin.open("Files\\input.txt", ios_base::in);
 	fout.open("Files\\output.txt", ios_base::out | ios_base::trunc);
 
-	if (!fin.is_open())
-	{
-		cerr << "ERROR" << endl;
-		system("pause");
-		return NULL;
-	}
-
+	fin.open("Files\\input.txt", ios_base::in);
 	Copy(fin, fout);
-	Print(fin);
-
 	fin.close();
+
+	fin.open("Files\\input.txt", ios_base::in);
+	Print(fin, fout);
+	fin.close();
+
+	fin.open("Files\\input.txt", ios_base::in);
+	fout << "--------------------------------------------------" << endl;
+	cout << endl << "The number of sentences that contain an odd number of words: " << StringOddWords(fin, fout) << endl << endl;
+	fout << "The number of sentences that contain an odd number of words: " << StringOddWords(fin, fout) << endl;
+	fin.close();
+
+	fin.open("Files\\input.txt", ios_base::in);
+	WordDelete(fin, fout);
+	fin.close();
+
+	cout << endl;
+
 	fout.close();
 	system("pause");
 	return NULL;
