e9e167d8c91f6c1558a15158dfbffdba239bdd43
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-13 10:32:32 +0200

    Lab-5.2 Function Add();

--- TwoWeekLab-5.2/Source.cpp
+++ TwoWeekLab-5.2/Source.cpp
@@ -12,13 +12,72 @@ struct ITINERARY
 	double DISTANCE;		//- відстань у кілометрах.
 };
 
+inline void Menu()
+{
+	cout << "Select action from menu:" << endl
+		<< "\t MENU" << endl
+		<< "1 - Adjustments record;" << endl
+		<< "2 - Search for information on various fields;" << endl
+		<< "3 - Adding items to the end of the database;" << endl
+		<< "4 - Removing information from the database." << endl
+		<< "0 - Exit" << endl;
+}
+
+/*	1 - коригування обраного запису файлу	*/
+/*	2 - пошук інформації за різними полями	*/
+
+/*	3 - додавання записів у кінець бази даних	*/
+void Add(ITINERARY& A)
+{
+	ofstream fout("database.txt", ios_base::app);
+	cout << "Endter beginning of route: ";
+	cin >> A.FIRST;
+	fout << A.FIRST << endl;
+	cout << "Enter the end of route: ";
+	cin >> A.FINAL;
+	fout << A.FINAL << endl;
+	cout << "Enter number of route: ";
+	cin >> A.NUM;
+	fout << A.NUM << endl;
+	cout << "Enter distance of route: ";
+	cin >> A.DISTANCE;
+	fout << A.DISTANCE << endl << endl;
+	fout.close();
+}
+
+/*	4 - вилучення інформації з бази даних	*/
+
 int main()
 {
-	fstream database;
-	database.open("database.txt", ios_base::in | ios_base::out | ios_base::binary);
+	ITINERARY bus;
+	int ch;
 
-	
-	database.close();
+	do {
+		Menu();
+		cout << endl << " >  ";
+		cin >> ch;
+		system("cls");
+		switch (ch)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			Add(bus);
+			break;
+		case 4:
+			break;
+		case 0:
+			return NULL;
+		default:
+			cerr << "Incorrect input!" << endl;
+			system("pause");
+			break;
+		}
+		system("cls");
+	} while (ch != 0);
+
 	system("pause");
 	return NULL;
 }
