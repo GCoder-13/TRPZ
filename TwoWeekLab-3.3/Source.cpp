598d2736bc86d698bf44387382dbe84c09f2eb8b
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-14 22:49:36 +0200

    Lab-3.3 Update and finish function ordDelete()

--- TwoWeekLab-3.3/Source.cpp
+++ TwoWeekLab-3.3/Source.cpp
@@ -6,10 +6,6 @@ using namespace std;
 void StrAndWord(string& str)
 {
 	int word = 1, nStr = 0;
-	if (str[str.length() - 1] == ' ')
-		str[str.length() - 1] = '\0';
-	else if (str[str.length() - 1] != ' ' && (str[str.length() - 1] != '.' || str[str.length() - 1] != '!' || str[str.length() - 1] != '?'))
-		str += '.';
 
 	for (int i(1); i < str.length(); i++)
 	{
@@ -63,9 +59,8 @@ void StringLength(string& str)
 				st = temp;
 				fn = i;
 			}
-			
 
-			while(str[i] == ' ' || str[i] == '.' || str[i] == '!' || str[i] == '?')
+			while (str[i] == ' ' || str[i] == '.' || str[i] == '!' || str[i] == '?')
 				temp = ++i;
 		}
 	}
@@ -76,9 +71,26 @@ void StringLength(string& str)
 void WordDelete(string& str)
 {
 	int st = 0, fn;
+
 	for (int i(st); i < str.length(); i++)
 	{
+		while (str[st] == ' ' || str[st] == '.' || str[st] == '!' || str[st] == '?')
+			i = ++st;
 
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '!' || str[i] == '?')
+		{
+			fn = i - 1;
+			while (str[fn] == ' ' || str[fn] == '.' || str[fn] == '!' || str[fn] == '?')
+				fn--;
+
+			if (str[fn - 1] == 'a' || str[fn - 1] == 'A' || str[fn - 1] == 'e' || str[fn - 1] == 'E' || str[fn - 1] == 'i' ||
+				str[fn - 1] == 'I' || str[fn - 1] == 'o' || str[fn - 1] == 'O' || str[fn - 1] == 'u' || str[fn - 1] == 'U')
+			{
+				str.erase(st, (fn - st + 1));
+				i = st;
+			}
+			else st = i + 1;
+		}
 	}
 }
 
@@ -88,20 +100,22 @@ int main()
 
 	cout << "Enter string:" << endl << " >  ";
 	getline(cin, str);
-	cout << endl;
 
-	if (str.empty())
-	{
-		cout << "You do not enter string!" << endl;
-		system("pause");
-		return NULL;
-	}
+	if (str[str.length() - 1] == ' ')
+		str[str.length() - 1] = '\0';
+	else if (str[str.length() - 1] != '.' && str[str.length() - 1] != '!' && str[str.length() - 1] != '?')
+		str.append(1, '.');
 
 	StrAndWord(str);
 	cout << endl << "The shortest sentence:" << endl;
 	StringLength(str);
-	cout << endl;
-	
+
+	cout << endl << endl << "Delete words:" << endl;
+	WordDelete(str);
+	if (str.empty())
+		cout << "All the words removed" << endl;
+	else cout << str << endl;
+
 	system("pause");
 	return NULL;
 }
