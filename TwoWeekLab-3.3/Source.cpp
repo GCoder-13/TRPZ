22bf355fae5eec2c90b806263952f814ab8d8054
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-14 10:52:36 +0200

    Lab-3.3 Corretion errors and improve program

--- TwoWeekLab-3.3/Source.cpp
+++ TwoWeekLab-3.3/Source.cpp
@@ -3,10 +3,51 @@
 
 using namespace std;
 
+void StrAndWord(string& str)
+{
+	int word = 1, nStr = 0;
+	if (str[str.length() - 1] == ' ')
+		str[str.length() - 1] = '\0';
+	else if (str[str.length() - 1] != ' ' && (str[str.length() - 1] != '.' || str[str.length() - 1] != '!' || str[str.length() - 1] != '?'))
+		str += '.';
+
+	for (int i(1); i < str.length(); i++)
+	{
+		if (i - 1 == 0 && str[i - 1] != ' ' && str[i] == ' ')
+			word++;
+		else if ((str[i] == ' ' && str[i - 1] == ' ') || str[i] == ',' || str[i] == ';' || str[i] == ':')
+			continue;
+		else if (str[i] == ' ')
+			word++;
+
+		if (str[i] == '.' || str[i] == '!' || str[i] == '?' || i == str.length() - 1)
+		{
+			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?')
+				word--;
+			nStr++;
+			if (word != 0)
+				cout << "String " << nStr << ": " << word << " words" << endl;
+			else nStr--;
+
+			if (str[i + 1] != ' ')
+				word = 1;
+			else word = 0;
+		}
+	}
+}
+
+void StringLength(string& str)
+{
+	int length = 0;
+	for (int i(0); i < str.length(); i++)
+	{
+
+	}
+}
+
 int main()
 {
 	string str;
-	int word = 1, nStr = 0;
 
 	cout << "Enter string:" << endl << " >  ";
 	getline(cin, str);
@@ -19,25 +60,8 @@ int main()
 		return NULL;
 	}
 
-	for (int i(1); i < str.length(); i++)
-	{
-		if (i - 1 == 0 && str[i - 1] != ' ' && str[i] == ' ')
-			word++;
-		else if ((str[i] == ' ' && str[i - 1] == ' ') || str[i] == ',' || str[i] == ';' || str[i] == ':')
-			continue;
-		else if (str[i] == ' ')
-			word++;			
+	StrAndWord(str);
 
-		if (str[i] == '.' || str[i] == '?' || str[i] == '!' || i == str.length()-1)
-		{
-			if(str[i-1] == ' ')
-				word--;
-			nStr++;
-			
-			cout << "String " << nStr << ": " << word << " words" << endl;
-			word = 0;
-		}
-	}
 	
 	system("pause");
 	return NULL;
