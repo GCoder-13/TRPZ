4604bbda60a0615159749552b1fd51edeceb7af1
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-02-23 20:57:25 +0200

    Lab-8 Correction library matrix

--- ThreeWeekLab-8/matrix.cpp
+++ ThreeWeekLab-8/matrix.cpp
@@ -1,41 +1,22 @@
 #include "matrix.h"
+#include "vector.h"
 #include <iostream>
 
 using namespace std;
-
-int * matrix::Create(const int n)
-{
-	int *arr = new int[n];
-	return arr;
-}
 
 int ** matrix::Create(const int n, const int m)
 {
 	int **arr = new int*[n];
 	for (int i = 0; i < n; i++)
-		arr[i] = Create(m);
+		arr[i] = vec::Create(m);
 	return arr;
-}
-
-void matrix::Delete(int *& arr)
-{
-	delete[] arr;
 }
 
 void matrix::Delete(int **& arr, const int n)
 {
 	for (int i = 0; i < n; i++)
-		Delete(arr[i]);
-	Delete(*arr);
-}
-
-void matrix::Input(int * arr, const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << " >  ";
-		cin >> arr[i];
-	}
+		vec::Delete(arr[i]);
+	vec::Delete(*arr);
 }
 
 void matrix::Input(int ** arr, const int n, const int m)
@@ -43,21 +24,15 @@ void matrix::Input(int ** arr, const int n, const int m)
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
-			Input(arr[i], m);
+			vec::Input(arr[i], m);
 		cout << endl << "------------------------------" << endl << endl;
 	}
-}
-
-void matrix::Add(int * arr, const int n, const int elem)
-{
-	for (int i = 0; i < n; i++)
-		arr[i] += elem;
 }
 
 void matrix::Add(int ** arr, const int n, const int m, const int elem)
 {
 	for (int i = 0; i < n; i++)
-		Add(arr[i], m, elem);
+		vec::Add(arr[i], m, elem);
 }
 
 int ** matrix::Transpose(const int ** arr, const int n, const int m)
--- ThreeWeekLab-8/matrix.h
+++ ThreeWeekLab-8/matrix.h
@@ -4,13 +4,9 @@
 
 namespace matrix
 {
-	int* Create(const int n);
 	int** Create(const int n, const int m);
-	void Delete(int *&arr);
 	void Delete(int **&arr, const int n);
-	void Input(int *arr, const int n);
 	void Input(int **arr, const int n, const int m);
-	void Add(int *arr, const int n, const int elem);
 	void Add(int **arr, const int n, const int m, const int elem);
 	int** Transpose(const int **arr, const int n, const int m);
 	int** Add(const int **arr1, const int **arr2, const int n, const int m);
